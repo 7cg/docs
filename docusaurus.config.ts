8ef48ee951685b8d4846e7a0861bc634a8d95d2c
Joel Calado <joelcalado@gmail.com>
2026-04-19 21:06:02 +0100

    Update social preview image

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -75,7 +75,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/7cg-social-card.jpg',
     colorMode: {
       respectPrefersColorScheme: true,
     },
